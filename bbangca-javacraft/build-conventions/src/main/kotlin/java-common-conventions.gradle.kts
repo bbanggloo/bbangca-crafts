07414fde15cdfa21ac5af7c55d1a4b44d5fc1dd4
bbangca <hsbae.dev@gmail.com>
2025-10-30 21:55:14 +0900

    build: junit5 bom 형태로 변경

--- bbangca-javacraft/build-conventions/src/main/kotlin/java-common-conventions.gradle.kts
+++ bbangca-javacraft/build-conventions/src/main/kotlin/java-common-conventions.gradle.kts
@@ -15,7 +15,9 @@ java {
 
 dependencies {
 	// JUnit5
-	testImplementation("org.junit.jupiter:junit-jupiter:6.0.0")
+	testImplementation(platform("org.junit:junit-bom:6.0.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	// AssertJ
 	testImplementation("org.assertj:assertj-core:3.26.3")
